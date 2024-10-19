938ca0694fee01b4a661a312e2d0c561edc6007e
divyanshucodes20 <155738552+divyanshucodes20@users.noreply.github.com>
2024-10-20 00:01:20 +0530

    Refactor package dependencies and add mini-svg-data-uri

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/Hero/HeroSection";
+import MusicSchoolTestimonials from "@/components/TestinomialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import { Main } from "next/document";
 
 export default function Home() {
   return (
 <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-<h1 className="text-2xl text-center">Next Music</h1>
 <HeroSection/>
+<FeaturedCourses/>
+<WhyChooseUs/>
+<MusicSchoolTestimonials/>
 </main>
   );
 }
