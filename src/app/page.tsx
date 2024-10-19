7ba65bee977ed259778f3f05413db3a2873d69bd
divyanshucodes20 <155738552+divyanshucodes20@users.noreply.github.com>
2024-10-20 00:25:12 +0530

    Refactor package dependencies and add simplex-noise library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/Hero/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestinomialCards";
+import UpComingWebinars from "@/components/UpComingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import { Main } from "next/document";
 
@@ -11,6 +13,8 @@ export default function Home() {
 <FeaturedCourses/>
 <WhyChooseUs/>
 <MusicSchoolTestimonials/>
+<UpComingWebinars/>
+<Instructors/>
 </main>
   );
 }
